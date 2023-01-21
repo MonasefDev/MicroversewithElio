30c820ac5d1eea983c60b1dcc648a4ebca61640c
Elio Miguel Gonzalez Stanzione <eliostanzione@hotmail.com>
2023-01-21 13:13:37 -0500

    added cart test to add the different products

--- js/index.js
+++ js/index.js
@@ -1,28 +1,22 @@
-// !! IMPORTANT README:
+let total = 0;
+let items = [];
+let values = [];
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+function createItemsList() {
+  let string = "";
+  for (let idx in items) {
+    string += `${items[idx]}: ${values[idx]}\n`;
+  }
+  return string;
+}
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
-"use strict";
+function addItem(id, value) {
+  if (items.includes(id)) {
+    console.log("This item was added already!");
+    return 1;
+  }
+  total += parseInt(value);
+  items.push(id);
+  values.push(value);
+  alert(`Your cart:\n${createItemsList()}\nYour total is: ${total}`);
+}
